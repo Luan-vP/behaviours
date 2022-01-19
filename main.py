63a371da471d726d2f719c712a53d8e6506c9090
Luan van Pletsen <luanvanpletsen@gmail.com>
2022-01-19 12:02:41 +0000

    Refactor

--- main.py
+++ main.py
@@ -2,22 +2,7 @@ from abc import ABC
 from dataclasses import dataclass
 import random
 from typing import List, Optional
-
-@dataclass
-class Behaviour(ABC):
-
-    goodness: float
-    category: str
-    effects: List[str]
-
-class Vegan(Behaviour):
-    """
-    Vegan behaviour
-    """
-
-    goodness = 0.8
-    category = "diet"
-    effects = ["healthy", "ethical"]
+from behaviour import *
 
 @dataclass
 class Person:

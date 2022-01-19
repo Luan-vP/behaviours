8317436cba01f026ff594354522f67d6f75710a7
Luan van Pletsen <luanvanpletsen@gmail.com>
2022-01-19 11:50:51 +0000

    Behaviours lead to speech

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from abc import ABC
 from dataclasses import dataclass
+import random
 from typing import List, Optional
 
-def main() --> None:
+@dataclass
+class Behaviour(ABC):
+
+    goodness: float
+    category: str
+    effects: List[str]
+
+class Vegan(Behaviour):
+    """
+    Vegan behaviour
+    """
+
+    goodness = 0.8
+    category = "diet"
+    effects = ["healthy", "ethical"]
+
+@dataclass
+class Person:
+    name: str
+    behaviours: List[Behaviour]
+
+    def speak(self) -> None:
+        behavioural_effects = [effect for behaviour in self.behaviours for effect in behaviour.effects]
+        print(f"I feel {random.sample(behavioural_effects, 1)[0]}")
+
+
+def main() -> None:
     """
     Main function.
     """
+
+    dave = Person(name="Dave", behaviours=[Vegan])
+    for i in range(10):
+        dave.speak()
     
-    class Behaviour(ABC):
-
-        goodness: float
-        category: str
-        exclusive: bool
-
-
-    @dataclass
-    class Person:
-        name: str
-        behaviours: List[Behaviour]
 
 if __name__ == "__main__":
     main()
-    # Do the real code
-    print('test')
+
 
     
 
